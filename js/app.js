c9e88243b09eff8dca1cf98d6b475d29b448d2b3
Gersonprogrammer <157986281+Gersonprogrammer@users.noreply.github.com>
2026-07-13 21:24:38 -0300

    Teste de transações

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+// ==============================
+// ALVESO - PERSONAL FINANCE
+// ==============================
+
+// Lista de transações
+const transacoes = [];
+transacoes.push({
+    descricao: "Salário",
+    valor: 5000,
+    tipo: "receita",
+    categoria: "Salário",
+    data: "13/07/2026"
+});
+
+transacoes.push({
+    descricao: "Supermercado",
+    valor: 380,
+    tipo: "despesa",
+    categoria: "Alimentação",
+    data: "13/07/2026"
+});
+console.log(transacoes);
+
+// Inicialização dos ícones
 lucide.createIcons();
